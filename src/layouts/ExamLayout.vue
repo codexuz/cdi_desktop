88bca519469cb8ecab3da25b1af37f97cb2b5e7b
codexuz <examonlineedumo@gmail.com>
2025-11-21 15:04:26 +0500

    "main"

---
+++ src/layouts/ExamLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex h-screen w-full flex-col">
+    <ExamHeader :title="title" />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import ExamHeader from '@/components/ExamHeader.vue'
+defineProps({
+  title: String,
+})
+</script>
